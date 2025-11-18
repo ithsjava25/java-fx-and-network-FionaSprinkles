2572b5fd86c7227d903ac3245136355c7159a6cd
Fiona Friberg <fionafriberg@outlook.com>
2025-11-18 20:56:49 +0100

    Fix Send messages

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -4,32 +4,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-
-import java.util.function.Consumer;
+import javafx.scene.control.TextField;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
  */
 public class HelloController {
 
-    private final HelloModel model = new HelloModel(new NtfyConnectionImpl() {
-        @Override
-        public boolean send(String message) {
-            return false;
-        }
+    private final HelloModel model = new HelloModel(new NtfyConnectionImpl());
 
-        @Override
-        public void receive(Consumer<NtfyMessageDto> messageHandler) {
-
-        }
-    });
     public ListView<NtfyMessageDto> messageView;
 
     @FXML
     private Label versionLabel;
 
     @FXML
+    private TextField inputField;
+
+    @FXML
     private void initialize() {
+        inputField.textProperty().bindBidirectional(model.messageToSendProperty());
         if (versionLabel != null) {
             versionLabel.setText(model.getGreeting());
         }
